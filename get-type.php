568479708080a7660a2cf44216d7aa52bacdfa08
Ellis Wilson <elliswilson91@googlemail.com>
2015-03-24 22:13:37 +0000

    initial commit file, to get product type

---
+++ get-type.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "sql203.byethost7.com";
+$username = "b7_16019404";
+$password = "123456789";
+$database = 'b7_16019404_pos';
+
+$type = htmlspecialchars($_GET["type"]);
+
+$link = mysqli_connect($servername, $username, $password, $database);
+
+$sql = "SELECT * FROM Products WHERE type = '$type'";
+$result = mysqli_query($link, $sql);
+$rows = array();
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        //echo "id: " . $row["UUID"].  "<br>";
+         $rows[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+
+print json_encode($rows);
+
+mysqli_close($link);
+?>	
