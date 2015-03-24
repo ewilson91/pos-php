05b6531dc72596d5313cbc14533517df98080979
Ellis Wilson <elliswilson91@googlemail.com>
2015-03-24 22:17:14 +0000

    Refractor

--- get-type.php
+++ get-type.php
@@ -14,11 +14,10 @@ $rows = array();
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        //echo "id: " . $row["UUID"].  "<br>";
          $rows[] = $row;
     }
 } else {
-    echo "0 results";
+    echo "no records";
 }
 
 print json_encode($rows);
